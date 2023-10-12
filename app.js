45cb483517448f84ff74c1bd22ee242831345df8
Labyrinthen <lavdim_milan@hotmail.com>
2023-10-12 23:24:52 +0200

    Hashed passwords and created users table with reg page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const session = require("express-session");
 const connectSqlite3 = require("connect-sqlite3");
 const cookieParser = require("cookie-parser");
 const sqlite3 = require("sqlite3");
+const { error } = require("console");
+const bcrypt = require("bcrypt");
 
 // defines handlebars engine
 app.engine("handlebars", engine());
@@ -54,7 +56,17 @@ const db = new sqlite3.Database("PORTFOLIOWEBSITE-LI.db");
 // CONTROLLER (THE BOSS)
 // defines routes "/"
 app.get("/", (req, res) => {
-  console.log("SESSION ", req.session);
+  console.log("SESSION: ", req.session);
+
+  // saltRounds = 12
+  // bcrypt.hash("i", saltRounds, function(err, hash){
+  //   if(err) {
+  //     console.log("Error encrypting the password: ", err)
+  //   } else{
+  //     console.log("Hashed password (GENERATE only ONCE): ", hash)
+  //   }
+  // });
+
   const model = {
     isLoggedIn: req.session.isLoggedIn,
     name: req.session.name,
@@ -73,12 +85,20 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  const model = {
-    isLoggedIn: req.session.isLoggedIn,
-    name: req.session.name,
-    isAdmin: req.session.isAdmin,
-  };
-  res.render("about.handlebars", model); // Pass the model to the template
+  db.all("SELECt * FROM LanguageSkills", (error, languageskils) => {
+    if (error) {
+      console.error("Error fetching Languageskills: ", error);
+      res.status(500).send("Error fetching Languageskills");
+    } else {
+      const model = {
+        isLoggedIn: req.session.isLoggedIn,
+        name: req.session.name,
+        isAdmin: req.session.isAdmin,
+        LanguageSkills: languageskils,
+      };
+      res.render("about.handlebars", model); // Pass the model to the template
+    }
+  });
 });
 
 app.get("/projects", (req, res) => {
@@ -259,26 +279,117 @@ app.get("/login", (req, res) => {
   res.render("login.handlebars", model);
 });
 
+db.run(
+  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, role TEXT NOT NULL)",
+  (error) => {
+    if (error) {
+      console.error("Error creating 'users' table: ", error);
+    } else {
+      console.log("Table 'users' created!");
+    }
+  }
+);
+
+app.get("/registration", (req, res) => {
+  const model = {
+    isLoggedIn: req.session.isLoggedIn,
+    name: req.session.name,
+    isAdmin: req.session.isAdmin,
+  };
+  res.render("registration.handlebars", model); // Redirect to the home page after logging out
+});
+
+app.post("/registration", (req, res) => {
+  const un = req.body.un;
+  const pw = req.body.pw;
+  const role = req.body.role; // Get the selected role
+
+  // Check if the user already exists
+  db.get("SELECT username FROM users WHERE username = ?", [un], (error, existingUser) => {
+    if (error) {
+      console.error("Error checking for existing user: ", error);
+      res.redirect("/registration"); // Handle registration failure
+    } else if (existingUser) {
+      console.log("User already exists.");
+      // Handle the case where the user already exists (e.g., display an error message)
+      res.redirect("/registration"); // Redirect back to the registration page
+    } else {
+      // Generate a salt
+      bcrypt.genSalt(12, function (saltError, salt) {
+        if (saltError) {
+          console.log("Error generating salt: ", saltError);
+          res.redirect("/registration"); // Handle registration failure
+        } else {
+          // Hash the user's password with the generated salt
+          bcrypt.hash(pw, salt, function (hashError, hash) {
+            if (hashError) {
+              console.log("Error in hashing password: ", hashError);
+              res.redirect("/registration"); // Handle registration failure
+            } else {
+              // Store the hashed password and role in the users table
+              db.run(
+                "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+                [un, hash, role],
+                (insertError) => {
+                  if (insertError) {
+                    console.log("Error inserting user data: ", insertError);
+                    res.redirect("/registration"); // Handle registration failure
+                  } else {
+                    console.log("User registered and password hashed!");
+                    res.redirect("/login"); // Redirect to the login page after successful registration
+                  }
+                }
+              );
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+
+// POST route for user login
 app.post("/login", (req, res) => {
   const un = req.body.un;
   const pw = req.body.pw;
 
-  if (un == "l" && pw == "i") {
-    console.log("Lavdim is logged in!");
-    req.session.isAdmin = true;
-    req.session.isLoggedIn = true;
-    req.session.name = "Lavdim";
-    res.redirect("/");
-  } else {
-    console.log("Bad user and/or bad password");
-    req.session.isAdmin = false;
-    req.session.isLoggedIn = false;
-    req.session.name = "";
-    res.redirect("/login");
-  }
-  console.log("LOGIN: ", un);
-  console.log("PASSWORD: ", pw);
+  db.get(
+    "SELECT username, password, role FROM users WHERE username = ?",
+    [un],
+    (error, result) => {
+      if (error) {
+        console.error("Error fetching user data: ", error);
+        res.redirect("/login"); // Handle login failure
+      } else if (result) {
+        const storedHashedPassword = result.password;
+
+        // Compare the entered password with the stored hashed password
+        bcrypt.compare(pw, storedHashedPassword, function (err, passwordMatch) {
+          if (err) {
+            console.error("Error comparing passwords: ", err);
+            res.redirect("/login"); // Handle login failure
+          } else if (passwordMatch) {
+            console.log("Password is correct. User is logged in!");
+            // Set user's role in the session
+            req.session.isAdmin = result.role === "admin";
+            req.session.isLoggedIn = true;
+            req.session.name = un;
+            res.redirect("/");
+          } else {
+            console.log("Password is incorrect. User is NOT logged in!");
+            res.redirect("/login"); // Handle login failure
+          }
+        });
+      } else {
+        console.log("User not found.");
+        res.redirect("/login"); // Handle login failure
+      }
+    }
+  );
 });
+
+
 
 // defines the final default route 404 NOT FOUND
 app.use(function (req, res) {
@@ -290,133 +401,132 @@ app.listen(port, () => {
   console.log(`Server running and listening on port ${port}...`);
 });
 
+
+
 // creates table projects at startup
+
 db.run(
-  "CREATE TABLE IF NOT EXISTS projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgURL TEXT NOT NULL)",
+  "CREATE TABLE IF NOT EXISTS projects (pid INTEGER PRIMARY KEY AUTOINCREMENT, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgURL TEXT NOT NULL)",
   (error) => {
     if (error) {
       // tests error: display error
-      console.log("ERROR: ", error);
+      console.log("Table 'projects' already exists:", error);
     } else {
       // tests error: no error, the table has been created
       console.log("---> Table projects created!");
-      const projects = [
-        {
-          id: "1",
-          name: "Cyberpunk tiger",
-          type: "other",
-          desc: "Cool art of a tiger in the future",
-          year: 2022,
-          url: "/img/a.png",
-        },
-        {
-          id: "2",
-          name: "Father and son",
-          type: "other",
-          desc: "Father and son in the desert praying with the moon in the background ",
-          year: 2021,
-          url: "/img/b.png",
-        },
-        {
-          id: "3",
-          name: "Ai jungle",
-          type: "other",
-          desc: "exploring ai jungle ",
-          year: 2020,
-          url: "/img/c.png",
-        },
-        {
-          id: "4",
-          name: "cool ai art",
-          desc: "man ai",
-          year: 2023,
-          type: "other",
-          url: "/img/d.png",
-        },
-        {
-          id: "5",
-          name: "cyberpunk cowboy",
-          desc: "cowboy in 2042",
-          year: 2023,
-          type: "other",
-          url: "/img/e.png",
-        },
-      ];
+      const projects = [];
 
-      // inserts projects
-      projects.forEach((oneProject) => {
-        db.run(
-          "INSERT INTO projects (pid, pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?, ?)",
-          [
-            oneProject.id,
-            oneProject.name,
-            oneProject.year,
-            oneProject.desc,
-            oneProject.type,
-            oneProject.url,
-          ],
-          (error) => {
+      function projectExists(project, callback) {
+        db.get(
+          "SELECT COUNT(*) as count FROM projects WHERE pname = ? AND pyear = ? AND pdesc = ?",
+          [project.name, project.year, project.desc],
+          (error, result) => {
             if (error) {
-              console.log("ERROR: ", error);
+              callback(error, null);
             } else {
-              console.log("Line added into the projects table!");
+              const count = result.count;
+              callback(null, count > 0);
             }
           }
         );
+      }
+
+      // inserts projects
+      projects.forEach((oneProject) => {
+        projectExists(oneProject, (error, exists) => {
+          if (error) {
+            console.log("ERROR: ", error);
+          } else {
+            if (!exists) {
+              // Only insert the project if it doesn't already exist
+              db.run(
+                "INSERT INTO projects (pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?)",
+                [
+                  oneProject.name,
+                  oneProject.year,
+                  oneProject.desc,
+                  oneProject.type,
+                  oneProject.url,
+                ],
+                (insertError) => {
+                  if (insertError) {
+                    console.log("ERROR: ", insertError);
+                  } else {
+                    console.log("Line added into the projects table!");
+                  }
+                }
+              );
+            } else {
+              console.log("Project already exists:", oneProject.name);
+            }
+          }
+        });
       });
     }
   }
 );
 
-// creates skills projects at startup
+// creates skills at startup
 db.run(
-  "CREATE TABLE IF NOT EXISTS skills (sid INTEGER PRIMARY KEY, sname TEXT NOT NULL, sdesc TEXT NOT NULL, stype TEXT NOT NULL)",
+  "CREATE TABLE IF NOT EXISTS skills (sid INTEGER PRIMARY KEY AUTOINCREMENT, sname TEXT NOT NULL, sdesc TEXT NOT NULL, stype TEXT NOT NULL)",
   (error) => {
     if (error) {
       // tests error: display error
-      console.log("ERROR: ", error);
+      console.log("Table 'skills' already exists:", error);
     } else {
       // tests error: no error, the table has been created
       console.log("---> Table skills created!");
       const skills = [
         {
-          id: "1",
           name: "c++",
           type: "Programming language",
           desc: "Programming software",
         },
         {
-          id: "2",
           name: "Sql",
           type: "Database Programming language",
           desc: "Programming databases.",
         },
-        {
-          id: "3",
-          name: "html/css/javascript",
-          type: "Programming language",
-          desc: "Programming websites",
-        },
-        {
-          id: "4",
-          name: "wix",
-          type: "website maker",
-          desc: "Making websites without coding",
-        },
       ];
-      // inserts skills
-      skills.forEach((oneSkill) => {
-        db.run(
-          "INSERT INTO skills (sid, sname, sdesc, stype) VALUES (?, ?, ?, ?)",
-          [oneSkill.id, oneSkill.name, oneSkill.desc, oneSkill.type],
-          (error) => {
+
+      function skillExists(skill, callback) {
+        db.get(
+          "SELECT COUNT(*) as count FROM skills WHERE sname = ?",
+          [skill.name],
+          (error, result) => {
             if (error) {
-              console.log("ERROR: ", error);
+              callback(error, null);
             } else {
-              console.log("Line added into the skills table!");
+              const count = result.count;
+              callback(null, count > 0);
             }
           }
         );
+      }
+      // inserts skills
+      skills.forEach((oneSkill) => {
+        skillExists(oneSkill, (error, exists) => {
+          if (error) {
+            console.log("ERROR: ", error);
+          } else {
+            if (!exists) {
+              // Only insert the skill if it doesn't already exist
+              db.run(
+                "INSERT INTO skills (sname, sdesc, stype) VALUES (?, ?, ?)",
+                [oneSkill.name, oneSkill.desc, oneSkill.type],
+                (insertError) => {
+                  if (insertError) {
+                    console.log("ERROR: ", insertError);
+                  } else {
+                    console.log("Line added into the skills table!");
+                  }
+                }
+              );
+            } else {
+              console.log("Skill already exists:", oneSkill.name);
+            }
+          }
+        });
       });
     }
   }
@@ -424,36 +534,62 @@ db.run(
 
 // creates table LanguageSkills at startup
 db.run(
-  "CREATE TABLE IF NOT EXISTS LanguageSkills (lgid INTEGER PRIMARY KEY, lname TEXT NOT NULL, lamount TEXT NOT NULL)",
+  "CREATE TABLE IF NOT EXISTS LanguageSkills (lgid INTEGER PRIMARY KEY AUTOINCREMENT, lname TEXT NOT NULL, lamount TEXT NOT NULL)",
   (error) => {
     if (error) {
       // tests error: display error
-      console.log("ERROR: ", error);
+      console.log("Table 'LanguageSkills' already exists:", error);
     } else {
       // tests error: no error, the table has been created
       console.log("---> Table LanguageSkills created!");
       const LanguageSkills = [
-        { lgid: "1", lname: "Swedish", lamount: "fluent" },
-        { lgid: "2", lname: "English", lamount: "fluent" },
-        { lgid: "3", lname: "Albanian", lamount: "good" },
+        { lname: "Swedish", lamount: "fluent" },
+        { lname: "English", lamount: "fluent" },
+        { lname: "Albanian", lamount: "good" },
       ];
-      // inserts LanguageSkills
-      LanguageSkills.forEach((oneLanguageSkill) => {
-        db.run(
-          "INSERT INTO LanguageSkills (lgid, lname, lamount) VALUES (?, ?, ?)",
-          [
-            oneLanguageSkill.lgid,
-            oneLanguageSkill.lname,
-            oneLanguageSkill.lamount,
-          ],
-          (error) => {
+
+      function languageSkillExists(skill, callback) {
+        db.get(
+          "SELECT COUNT(*) as count FROM LanguageSkills WHERE lname = ?",
+          [skill.lname],
+          (error, result) => {
             if (error) {
-              console.log("ERROR: ", error);
+              callback(error, null);
             } else {
-              console.log("Line added into the LanguageSkills table!");
+              const count = result.count;
+              callback(null, count > 0);
             }
           }
         );
+      }
+
+      // inserts LanguageSkills
+      LanguageSkills.forEach((oneLanguageSkill) => {
+        languageSkillExists(oneLanguageSkill, (error, exists) => {
+          if (error) {
+            console.log("ERROR: ", error);
+          } else {
+            if (!exists) {
+              // Only insert the language skill if it doesn't already exist
+              db.run(
+                "INSERT INTO LanguageSkills (lname, lamount) VALUES (?, ?)",
+                [oneLanguageSkill.lname, oneLanguageSkill.lamount],
+                (insertError) => {
+                  if (insertError) {
+                    console.log("ERROR: ", insertError);
+                  } else {
+                    console.log("Line added into the LanguageSkills table!");
+                  }
+                }
+              );
+            } else {
+              console.log(
+                "Language skill already exists:",
+                oneLanguageSkill.lname
+              );
+            }
+          }
+        });
       });
     }
   }
